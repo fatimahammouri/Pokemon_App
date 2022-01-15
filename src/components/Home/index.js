df80205b899f8565a6004646ed856ee1846c461d
fatimaAlhammouri <fatimahammouri@users.noreply.github.com>
2022-01-15 07:30:30 -0800

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+
+export const Home = () => {
+    const [pokemons, setPokemons] = useState([]);
+    
+    return (
+        <div>
+        
+        </div>
+    )
+}
