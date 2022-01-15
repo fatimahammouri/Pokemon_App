183bfa5b90377d874be3bcaac3354ae79b94be37
fatimaAlhammouri <fatimahammouri@users.noreply.github.com>
2022-01-15 08:00:12 -0800

    get pokemons from web API

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useState, useEffect } from "react";
-
+import  axios from "axios";
+ 
 export const Home = () => {
     const [pokemons, setPokemons] = useState([]);
     
+    useEffect(() => {
+        axios.get("https://pokeapi.co/api/v2/pokemon")
+        .then((res) => {
+            console.log(res)
+            setPokemons(res.data.results) 
+        })  
+    }, []);
+
     return (
         <div>
-        
+            {pokemons.map((pokemon) => {return <li>{pokemon.name}</li>})}
         </div>
     )
 }
