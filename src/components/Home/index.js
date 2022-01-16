70e3edab66c89f41bc908a12d1f0e82ab22797da
fatimaAlhammouri <fatimahammouri@users.noreply.github.com>
2022-01-16 00:38:49 -0800

    setup redux

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import  axios from "axios";
+
  
 export const Home = () => {
     const [pokemons, setPokemons] = useState([]);
-    
+    const [next, setNext] = useState("")
+
+
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon")
         .then((res) => {
             console.log(res)
-            setPokemons(res.data.results) 
-        })  
+            console.log(res.data.next)
+            setPokemons(res.data.results)
+            setNext(res.data.next) 
+        })
+        .catch(() => {console.log("Error")})
     }, []);
+
+    const handleNextPageBtn = () => {
+        alert("clicked the button")
+        axios.get(next)
+        .then((res) => {
+            setNext(res.data.next) 
+            setPokemons(res.data.results)
+        })
+    }
 
     return (
         <div>
             {pokemons.map((pokemon) => {return <li>{pokemon.name}</li>})}
+            <button onClick={handleNextPageBtn}> Next </button>
         </div>
     )
 }
---
+++ src/reducers/store.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore } from "redux";
+import { } from "./";
+ 
+const reducers = combineReducers({})
+const store = createStore(reducers);
+
+export default store;
